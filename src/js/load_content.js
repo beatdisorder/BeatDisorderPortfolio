d023e2bb130f808ece6d139b5819c046075d8ef6
fgonzalezalvarez <frgonzalezalvarez@gmail.com>
2017-04-21 13:44:54 -0300

    * Se agrega parallax scroll * Se agrega carga dinamica de marketing (Desde JSON) * Se agrega go con animacion

--- src/js/load_content.js
+++ src/js/load_content.js
@@ -2,20 +2,23 @@
 function chargeContent() {
   let selectedLanguaje = getSelectedLanguaje();
   chargeCarousel(selectedLanguaje);
+  chargeMarketing(selectedLanguaje);
 }
 
-// function to reload carousel content from json
+// function to load carousel content from json
 function chargeCarousel(selectedLanguaje){
   $.getJSON("data/json/items/carousel." + selectedLanguaje + ".json?nocache=" + (new Date()).getTime(), function(json){
     // delete child nodes
-    $( ".carousel-indicators" ).empty();
-    $( ".carousel-inner" ).empty();
+    let caruselIndicator = $( ".carousel-indicators" );
+    let caruselInner = $( ".carousel-inner" );
+    caruselIndicator.empty()
+    caruselInner.empty();
     // add new content
     for(let i = 0 ; i < json.length ; i++){
       let object = json[i];
-      $( ".carousel-indicators" ).append("<li data-target=\"#portfolio-carousel\" data-slide-to=\"" + i + "\"" + ( i === 0 ? " class=\"active\"></li>" : "></li>"));
+      caruselIndicator.append("<li data-target=\"#portfolio-carousel\" data-slide-to=\"" + i + "\"" + ( i === 0 ? " class=\"active\"></li>" : "></li>"));
 
-      $( ".carousel-inner" ).append(
+      caruselInner.append(
         "<div class=\"item "
       // define active if is index 0
       + ( i === 0 ? " active\"" : "\"")
@@ -30,6 +33,28 @@ function chargeCarousel(selectedLanguaje){
   });
 }
 
+// function to load  marketing icons info
+function chargeMarketing(selectedLanguaje){
+  $.getJSON("data/json/items/marketing." + selectedLanguaje + ".json?nocache=" + (new Date()).getTime(), function(json){
+    // delete child nodes
+    let content = $( ".marketing .container" );
+    content.empty();
 
-// first invoke chargeContent
-chargeContent();
+    // define columns
+    let columns = 3;
+    // concatenator content
+    let fragment = "";
+    // add new content
+    for(let i = 0 ; i < json.length ; i++){
+      let object = json[i];
+        fragment += ((i === 0 || i % columns === 0) ? "<div class=\"row\" >" : "")
+        + "<div class=\"col-md-4\">"
+        + (object.glyphicon !== undefined ? "<span class=\"glyphicon " + object.glyphicon + "\" ></span>" : "")
+        + "<h2>" + object.title + "</h2>"
+        + "<p>" + object.text + "</p>"
+        + "</div>"
+        + (((i + 1) % columns === 0) || json.length === (i + 1) ? "</div>" : "");
+    }
+    content.append(fragment);
+  });
+}
